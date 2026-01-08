272d2a93d02a29c83f9db275546f228a2f55bd1d
Sujalgopani <sujalgopani34@gmail.com>
2026-01-08 18:10:42 +0530

    lerning fetch simple api paramaeter wise fetching, encode pass data, header, server responce handle, reposrtprogress geting, handle fail api, timout retry catchError, advanced option : fetch option, priority, cashing and many more

--- src/app/Http/configservice.ts
+++ src/app/Http/configservice.ts
@@ -1,15 +1,87 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpParams,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError, timeout } from 'rxjs';
+import { customencoding } from './customencodinghttps';
+import { ObjectEncodingOptions, stat } from 'fs';
+import { time } from 'console';
+import { timeoutProvider } from 'rxjs/internal/scheduler/timeoutProvider';
 
 @Injectable({ providedIn: 'root' })
 export class userservice {
   constructor(private http: HttpClient) {}
+  private api = 'https://jsonplaceholder.typicode.com/users';
 
-  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  getuser(): Observable<any> {
+    return this.http.get(this.api);
+  }
 
-  getUsers(sid : number): Observable<any> {
-    return this.http.get(this.apiUrl,{params:{id:sid}});
+  gtbyid(id: number): Observable<any> {
+    return this.http.get(this.api, { params: { id: id } });
+    //return this.http.get(`${this.api}\${id}`); //optional
+  }
+
+  checkencode(email: any) {
+    const params = new HttpParams({ encoder: new customencoding() }).set(`email`, email);
+    return this.http.get('https://httpbin.org/get', { params });
+  }
+
+  getbyemailEncoded(email: any) {
+    const headers = {
+      'Made-By': 'Sujal-Gopani',
+    };
+    const params = new HttpParams({ encoder: new customencoding() }).set(`email`, email);
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/users', { params, headers })
+      .subscribe(console.log);
+  }
+
+  getserverresponceevent() {
+    const headers = {
+      Sujal: 'Angular',
+    };
+    return this.http.get('https://jsonplaceholder.typicode.com/users?email=Sincere@april.biz', {
+      headers,
+      observe: 'response',
+      reportProgress: true,
+      timeout: 2000,
+    });
+  }
+
+  uploadFile(file: File): Observable<HttpEvent<any>> {
+    const formdata = new FormData();
+    formdata.append('file', file);
+    const uploadapi = 'https://api.escuelajs.co/api/v1/files/upload';
+
+    return this.http.post(uploadapi, formdata, {
+      reportProgress: true,
+      observe: 'events' as const,
+    });
+  }
+
+  // error fail then handle
+  getdatawitherrorcheck(): Observable<any> {
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .pipe(timeout(147), retry(2), catchError(this.handleError));
+  }
+
+  private handleError(erro: HttpErrorResponse) {
+    let msg = 'Something Went Wrong !';
+    if (erro.status === 0) {
+      msg = 'Network error or Server unreachanble';
+    } else {
+      msg = 'Server Side Error !';
+    }
+
+    return throwError(() => ({
+      msg,
+      status: erro.status,
+    }));
   }
 }
--- src/app/Http/httpreq/httpreq.ts
+++ src/app/Http/httpreq/httpreq.ts
@@ -1,26 +1,135 @@
 import { Component, numberAttribute } from '@angular/core';
 import { userservice } from '../configservice';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { domainToASCII } from 'url';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-httpreq',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './httpreq.html',
   styleUrl: './httpreq.css',
 })
-
 export class Httpreq {
-  constructor(private usersService: userservice) {}
-  apiform= new FormGroup({
-    ids :new FormControl('')
-  })
-   loadUsers() {
-    const id = Number(this.apiform.get('ids')?.value);
-    this.usersService.getUsers(id).subscribe(data => {
-      if(data.length ===0){
-        console.log(404)
-      }else
-      console.log('Users from API:', data);
+  constructor(private userservice: userservice) {}
+  // fetch json by click button
+  getalluser() {
+    this.userservice.getuser().subscribe((data) => {
+      if (!data) console.log(404);
+      else console.log(data);
     });
+  }
+
+  // fetch json by id
+  apiform = new FormGroup({
+    uid: new FormControl(),
+  });
+
+  getuserbyid() {
+    const id = this.apiform.get('uid')?.value;
+    return this.userservice.gtbyid(id).subscribe((data) => {
+      if (data.length === 0) console.log(404 + ' User Not Found !');
+      else console.log(data);
+    });
+  }
+
+  // for testing and see encoded email or any string
+  encodeform = new FormGroup({
+    encodeinput: new FormControl(''),
+  });
+
+  // PASSED : SUJAL GOPANI+2005 @GMAIL.COM
+  // ANGULAR CONSIDER : SUJAL GOPANI%2B2005 %40GMAIL.COM
+  demoencode() {
+    this.userservice.checkencode(this.encodeform.get('encodeinput')?.value).subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  // fetch json by encode form email
+  getbyemail() {
+    this.userservice.getbyemailEncoded(this.encodeform.get('encodeinput')?.value);
+    // .subscribe(data=>{
+    //   console.log(data)
+    // })
+  }
+
+  getserverresponse() {
+    return this.userservice.getserverresponceevent().subscribe((data) => {
+      console.log(data.status);
+      console.log(data.body);
+      console.log(data.headers);
+    });
+  }
+
+  // upload file and see the progress
+  uploadProgress = 0;
+  uploadStatus = '';
+
+  progresscount = 0;
+  progressstatus = '';
+  progreeIsVisible = false;
+
+  addfile(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+    this.uploadfile(file);
+  }
+
+  uploadfile(file: File) {
+    this.userservice.uploadFile(file).subscribe((event) => {
+      switch (event.type) {
+        case HttpEventType.Sent:
+          this.progressstatus = 'Upload Loading....!';
+          this.progreeIsVisible = true;
+          break;
+
+        case HttpEventType.UploadProgress:
+          this.progresscount = Math.floor((event.loaded / (event.total ?? 1)) * 100);
+          break;
+
+        case HttpEventType.Response:
+          this.progressstatus = 'Uploaded Data SuccessFully Done !';
+          console.log(event.body);
+          this.progreeIsVisible = false;
+
+          break;
+      }
+    });
+  }
+
+  // api response fail
+  ngOnInit(){
+    this.geterrocheck()
+  }
+
+  loading = false;
+  errormsg = '';
+  edata : string[] = [];
+  geterrocheck(){
+    this.loading = true;
+    this.errormsg= '';
+
+    this.userservice.getdatawitherrorcheck().subscribe({
+      next:data=>{
+
+          this.edata = data;
+          this.loading = false;
+          console.log(this.edata)
+      },
+      error:err=>{
+        this.loading= false;
+        this.errormsg= err.msg;
+      }
+    })
+  }
+
+  retry() {
+    this.geterrocheck();
   }
 }
